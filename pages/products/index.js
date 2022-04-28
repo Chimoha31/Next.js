01020e3640eae98e9d43050e4e7d4ea21ee50896
Chimoha31 <mokochii1108@gmail.com>
2022-04-27 19:32:37 -0700

    route

--- pages/products/index.js
+++ pages/products/index.js
@@ -1,7 +1,30 @@
+import Link from 'next/link';
+import styles from '../../styles/Home.module.css';
+
 const ProductsList = () => {
   return (
-    <div>
-      <h1>Products desu</h1>
+    <div className={styles.container}>
+      <main className={styles.main}>
+        <h2 className={styles.title}>Products List</h2>
+
+        <ul>
+          <li>
+            <Link href="/products/smartphone">
+              <a>SmartPhone</a>
+            </Link>
+          </li>
+          <li>
+            <Link href="/products/pc">
+              <a>Laptop</a>
+            </Link>
+          </li>
+          <li>
+            <Link href="/products/headphone">
+              <a>HeadPhone</a>
+            </Link>
+          </li>
+        </ul>
+      </main>
     </div>
   );
 }
