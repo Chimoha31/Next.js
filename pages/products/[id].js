adc5c63f04a0d7b24196b4829d4f7924a777561c
Chimoha31 <mokochii1108@gmail.com>
2022-04-27 19:53:01 -0700

    Get data

--- pages/products/[id].js
+++ pages/products/[id].js
@@ -1,14 +1,36 @@
+import Link from "next/link";
 import { useRouter } from "next/router";
 
-const Product= () => {
- const router =  useRouter();
-//  console.log(router.query.id);
- const {id} = router.query;
+// SSG
+export async function getStaticProps({ params }) {
+  const req = await fetch(`http://localhost:3000/${params.id}.json`);
+  const data = await req.json();
+
+  return {
+    props: {
+      product: data,
+    },
+  };
+}
+
+const Product = ({ product }) => {
+  const router = useRouter();
+  //  console.log(router.query.id);
+  const { id } = router.query;
+
   return (
-    <div>
-      {id}
+    <div className={styles.container}>
+      <main className={styles.main}>
+        <h1>{id} Page</h1>
+        <img src={product.image} width="300" height="400" />
+        <p>{product.name}</p>
+        <br />
+        <Link href="/products">
+          <a>To Products List</a>
+        </Link>
+      </main>
     </div>
   );
-}
+};
 
 export default Product;
